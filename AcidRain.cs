eb364c350dbf130395acda99bb97ab4b8476cf7d
Remod <desolationoutpostpve@gmail.com>
2021-10-19 06:25:14 -0500

    Various logic and config adjustments to hopefully improve operation.  Added general sleeper protection scale of 0.1, which is 10% of normal, regardless of clothing state.

--- AcidRain.cs
+++ AcidRain.cs
@@ -1,4 +1,3 @@
-﻿//#define DEBUG
 #region License (GPL v3)
 /*
     NextGenPVE - Prevent damage to players and objects in a Rust PVE environment
@@ -24,12 +23,13 @@
 using Network;
 using Oxide.Core;
 using Oxide.Core.Libraries.Covalence;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Acid Rain", "RFC1920", "1.0.6")]
+    [Info("Acid Rain", "RFC1920", "1.0.7")]
     [Description("The rain can kill you - take cover!")]
 
     class AcidRain : RustPlugin
@@ -37,7 +37,7 @@ namespace Oxide.Plugins
         private ConfigData configData;
         public static AcidRain Instance = null;
         public List<ulong> protectedPlayers = new List<ulong>();
-        private bool enabled = true;
+        private bool PluginEnabled;
         private const string permAdmin = "acidrain.admin";
 
         #region Message
@@ -57,7 +57,7 @@ namespace Oxide.Plugins
                 ["disabled"] = "Acid Rain has been DISabled.",
                 ["innoculated"] = "All players have been innoculated!",
                 ["innoculation"] = "You have been innoculated!",
-                ["protected"] = "You have {0} minutes to protect yourself from the ACID RAIN!",
+                ["protectedm"] = "You have about {0} minutes to protect yourself from the ACID RAIN!",
                 ["recovered"] = "You have recovered from the ACID RAIN."
             }, this);
         }
@@ -71,13 +71,23 @@ namespace Oxide.Plugins
             AddCovalenceCommand("arstart", "CmdEnable");
 
             LoadConfigVariables();
-            enabled = configData.Options.EnableOnLoad;
+            PluginEnabled = configData.Options.EnableOnLoad;
 
-            foreach (var pl in BasePlayer.activePlayerList)
+            if (PluginEnabled)
             {
-                if (!pl.gameObject.GetComponent<AcidRads>())
+                foreach (var pl in BasePlayer.activePlayerList)
                 {
+                    if (configData.Options.debug) Puts($"Adding object to active player {pl.UserIDString}");
                     AddRadComponent(pl);
+                }
+
+                if (configData.Options.damageSleepers)
+                {
+                    foreach (var pl in BasePlayer.sleepingPlayerList)
+                    {
+                        if (configData.Options.debug) Puts($"Adding object to sleeper {pl.UserIDString}");
+                        AddRadComponent(pl);
+                    }
                 }
             }
         }
@@ -87,7 +97,14 @@ namespace Oxide.Plugins
         {
             foreach (var pl in BasePlayer.activePlayerList)
             {
-                if(pl.GetComponent<AcidRads>())
+                if (pl.GetComponent<AcidRads>())
+                {
+                    UnityEngine.Object.Destroy(pl.gameObject.GetComponent<AcidRads>());
+                }
+            }
+            foreach (var pl in BasePlayer.sleepingPlayerList)
+            {
+                if (pl.gameObject.GetComponent<AcidRads>())
                 {
                     UnityEngine.Object.Destroy(pl.gameObject.GetComponent<AcidRads>());
                 }
@@ -96,33 +113,41 @@ namespace Oxide.Plugins
 
         private void OnPlayerConnected(BasePlayer player)
         {
-            if (!player.gameObject.GetComponent<AcidRads>())
+            if (PluginEnabled)
             {
                 AddRadComponent(player);
+
+                if (!protectedPlayers.Contains(player.userID))
+                {
+                    SendReply(player, Instance.Lang("beware", player.UserIDString));
+                }
             }
-            if (!protectedPlayers.Contains(player.userID))
+        }
+
+        private void OnPlayerDisconnected(BasePlayer player, string reason)
+        {
+            if (!configData.Options.damageSleepers)
             {
-                SendReply(player, Instance.Lang("beware", player.UserIDString));
+                var c = player.gameObject.GetComponent<AcidRads>();
+                if (c != null)
+                {
+                    UnityEngine.Object.Destroy(c);
+                }
             }
         }
 
         void OnUserRespawned(IPlayer player)
         {
             if (player == null) return;
-            Message(player, "protected", (configData.Options.protectionTimer / 60).ToString());
-            protectedPlayers.Add((player.Object as BasePlayer).userID);
-            timer.Once(configData.Options.protectionTimer, () =>
+            if (PluginEnabled)
             {
-                protectedPlayers.Remove((player.Object as BasePlayer).userID);
-                Message(player, "beware");
-            });
-        }
-
-        private void OnPlayerDisconnected(BasePlayer player, string reason)
-        {
-            if (player.gameObject.GetComponent<AcidRads>())
-            {
-                UnityEngine.Object.Destroy(player.gameObject.GetComponent<AcidRads>());
+                Message(player, "protectedm", (Math.Floor(configData.Options.protectionTimer / 60)).ToString());
+                protectedPlayers.Add((player.Object as BasePlayer).userID);
+                timer.Once(configData.Options.protectionTimer, () =>
+                {
+                    protectedPlayers.Remove((player.Object as BasePlayer).userID);
+                    Message(player, "beware");
+                });
             }
         }
         #endregion hooks
@@ -138,17 +163,22 @@ namespace Oxide.Plugins
         protected override void LoadDefaultConfig()
         {
             Puts("Creating new config file.");
-            var config = new ConfigData
+            ConfigData config = new ConfigData
             {
-                Version = Version,
+                Options = new Options()
+                {
+                    hilevelbump = 1f,
+                    hipoisonbump = 0.5f,
+                    lolevelbump = 0.2f,
+                    lopoisonbump = 0.1f,
+                    notifyTimer = 60f,
+                    protectionTimer = 300f,
+                    damageSleepers = false,
+                    EnableOnLoad = true,
+                    debug = false
+                },
+                Version = Version
             };
-            config.Options.hilevelbump = 1f;
-            config.Options.hipoisonbump = 0.5f;
-            config.Options.lolevelbump = 0.2f;
-            config.Options.lopoisonbump = 0.1f;
-            config.Options.notifyTimer = 60f;
-            config.Options.protectionTimer = 300f;
-            config.Options.EnableOnLoad = true;
 
             SaveConfig(config);
         }
@@ -159,8 +189,26 @@ namespace Oxide.Plugins
         #endregion
 
         #region helper
+        private void SendEffectTo(string effect, BasePlayer player)
+        {
+            if (player == null) return;
+
+            var EffectInstance = new Effect();
+            EffectInstance.Init(Effect.Type.Generic, player, 0, Vector3.up, Vector3.zero);
+            EffectInstance.pooledstringid = StringPool.Get(effect);
+            Net.sv.write.Start();
+            Net.sv.write.PacketID(Network.Message.Type.Effect);
+            EffectInstance.WriteToStream(Net.sv.write);
+            Net.sv.write.Send(new SendInfo(player.net.connection));
+            EffectInstance.Clear();
+        }
+
         private void AddRadComponent(BasePlayer player)
         {
+            if (player.gameObject.GetComponent<AcidRads>())
+            {
+                UnityEngine.Object.Destroy(player.gameObject.GetComponent<AcidRads>());
+            }
             var c = player.gameObject.AddComponent<AcidRads>();
             c.Options = new Options
             {
@@ -178,7 +226,7 @@ namespace Oxide.Plugins
         private void CmdDisable(IPlayer iplayer, string command, string[] args)
         {
             if (!iplayer.HasPermission(permAdmin)) { Message(iplayer, "notauthorized"); return; }
-            enabled = false;
+            PluginEnabled = false;
             Message(iplayer, "disabled");
         }
 
@@ -186,7 +234,7 @@ namespace Oxide.Plugins
         private void CmdEnable(IPlayer iplayer, string command, string[] args)
         {
             if (!iplayer.HasPermission(permAdmin)) { Message(iplayer, "notauthorized"); return; }
-            enabled = true;
+            PluginEnabled = true;
             Message(iplayer, "enabled");
         }
 
@@ -194,16 +242,15 @@ namespace Oxide.Plugins
         private void CmdInnoculate(IPlayer iplayer, string command, string[] args)
         {
             if (!iplayer.HasPermission(permAdmin)) { Message(iplayer, "notauthorized"); return; }
-            if(args.Length > 0 && args[0] == "stop")
+            if (args.Length > 0 && args[0] == "stop")
             {
-                enabled = false;
+                PluginEnabled = false;
                 Message(iplayer, "disabled");
             }
+
             foreach (var pl in BasePlayer.activePlayerList)
             {
-#if DEBUG
-                Puts($"Innoculating {pl.displayName}");
-#endif
+                if (configData.Options.debug) Puts($"Innoculating {pl.displayName}");
                 if (pl.isActiveAndEnabled)
                 {
                     SendEffectTo("assets/prefabs/tools/medical syringe/effects/inject_friend.prefab", pl);
@@ -212,32 +259,34 @@ namespace Oxide.Plugins
                     SendReply(pl, Instance.Lang("innoculation"));
                 }
             }
+
+            if (configData.Options.damageSleepers)
+            {
+                foreach (var pl in BasePlayer.sleepingPlayerList)
+                {
+                    if (configData.Options.debug) Puts($"Innoculating {pl.displayName}");
+                    if (pl.isActiveAndEnabled)
+                    {
+                        SendEffectTo("assets/prefabs/tools/medical syringe/effects/inject_friend.prefab", pl);
+                        if (pl.IsWounded()) pl.StopWounded();
+                        pl.Heal(100f);
+                        SendReply(pl, Instance.Lang("innoculation"));
+                    }
+                }
+            }
+
             Message(iplayer, "innoculated");
-        }
-
-        private void SendEffectTo(string effect, BasePlayer player)
-        {
-            if (player == null) return;
-
-            var EffectInstance = new Effect();
-            EffectInstance.Init(Effect.Type.Generic, player, 0, Vector3.up, Vector3.zero);
-            EffectInstance.pooledstringid = StringPool.Get(effect);
-            Net.sv.write.Start();
-            Net.sv.write.PacketID(Network.Message.Type.Effect);
-            EffectInstance.WriteToStream(Net.sv.write);
-            Net.sv.write.Send(new SendInfo(player.net.connection));
-            EffectInstance.Clear();
         }
         #endregion
 
         #region classes
         private class ConfigData
         {
-            public Options Options = new Options();
+            public Options Options;
             public VersionNumber Version;
         }
 
-        private class Options
+        public class Options
         {
             public float hilevelbump;
             public float hipoisonbump;
@@ -245,7 +294,9 @@ namespace Oxide.Plugins
             public float lopoisonbump;
             public float notifyTimer;
             public float protectionTimer;
-            public bool EnableOnLoad = true;
+            public bool damageSleepers;
+            public bool EnableOnLoad;
+            public bool debug;
         }
 
         class AcidRads : MonoBehaviour
@@ -267,7 +318,7 @@ namespace Oxide.Plugins
 
             private void NotifyTimer(bool start = false)
             {
-                if(timer != null && timer.Destroyed)
+                if (timer != null && timer.Destroyed)
                 {
                     notified = false;
                 }
@@ -279,13 +330,12 @@ namespace Oxide.Plugins
 
             private float CheckProtection()
             {
-                if (!Instance.enabled) return 0f;
+                if (!Instance.PluginEnabled) return 0f;
                 float scale = 1f;
+                string sleeping = "";
                 foreach (var item in player.inventory.containerWear.itemList)
                 {
-#if DEBUG
-                    Instance.Puts($"Player {player.displayName} wearing {item.info.name}");
-#endif
+                    if (Instance.configData.Options.debug) Instance.Puts($"Player {player.displayName} wearing {item.info.name}");
                     if (item.info.name.Contains("hat") || item.info.name.Contains("cap"))
                     {
                         scale *= 0.99f;
@@ -294,7 +344,7 @@ namespace Oxide.Plugins
                     {
                         scale *= 0.98f;
                     }
-                    if(item.info.name.Contains("Boot"))
+                    if (item.info.name.Contains("Boot"))
                     {
                         scale *= 0.97f;
                     }
@@ -302,7 +352,7 @@ namespace Oxide.Plugins
                     {
                         scale *= 0.96f;
                     }
-                    else if(item.info.name.Contains("pants"))
+                    else if (item.info.name.Contains("pants"))
                     {
                         scale *= 0.95f;
                     }
@@ -314,14 +364,17 @@ namespace Oxide.Plugins
                     {
                         scale *= 0.8f;
                     }
-                    if(item.info.name.Equals("Hazmat_Suit.item"))
+                    if (item.info.name.Equals("Hazmat_Suit.item"))
                     {
                         scale *= 0.5f;
                     }
-#if DEBUG
-                    Instance.Puts($"Player {player.displayName} total protection scale = {scale.ToString()}");
-#endif
                 }
+                if (player.IsSleeping())
+                {
+                    scale = 0.1f;
+                    sleeping = "(sleeping)";
+                }
+                if (Instance.configData.Options.debug) Instance.Puts($"Player {player.displayName}{sleeping} total protection scale = {scale.ToString()}");
                 return scale;
             }
 
@@ -335,6 +388,8 @@ namespace Oxide.Plugins
                 if (player.metabolism.wetness.lastValue > 0 && !Instance.protectedPlayers.Contains(player.userID))
                 {
                     float scale = CheckProtection();
+                    if (Instance.configData.Options.debug) Instance.Puts($"Working on {player.UserIDString}, currentrain: {currentrain.ToString()}");
+
                     if (currentrain > 0.5f)
                     {
                         if (scale > 0)
@@ -354,7 +409,7 @@ namespace Oxide.Plugins
                         }
                     }
                 }
-                else if(player.metabolism.radiation_poison.lastValue > 0)
+                else if (player.metabolism.radiation_poison.lastValue > 0)
                 {
                     valid = true;
                     level *= 0.9f;
@@ -363,14 +418,15 @@ namespace Oxide.Plugins
 
                 if (valid && (level > 0 || poison > 0))
                 {
-#if DEBUG
-                    Instance.Puts($"Increasing player radiation for {player.displayName} by (Level/Poison {level}/{poison})");
-#endif
+                    if (Instance.configData.Options.debug) Instance.Puts($"Increasing player radiation for {player.displayName} by (Level/Poison {level}/{poison})");
                     player.metabolism.radiation_level.SetValue(player.metabolism.radiation_level.lastValue + level);
                     player.metabolism.radiation_poison.SetValue(player.metabolism.radiation_poison.lastValue + poison);
                     if (!notified)
                     {
-                        Instance.SendReply(player, Instance.Lang("seekshelter", player.UserIDString));
+                        if (!player.IsSleeping())
+                        {
+                            Instance.SendReply(player, Instance.Lang("seekshelter", player.UserIDString));
+                        }
                         NotifyTimer(true);
                         notified = true;
                     }
